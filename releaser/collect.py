c3543f4519ef952ac188c2071ec51891501362d6
MartinNose <liucmartin@qq.com>
2021-07-13 18:04:55 +0800

    add frameword

---
+++ releaser/collect.py
@@ -0,0 +1,15 @@
+from github import Github
+
+def list(milestone : str , labels : list, repos : list):
+    print(milestone, labels, repos)
+    
+
+
+    pass
+
+
+def pull(milestone : str, labels : list, repos : list):
+    print(milestone, labels, repos)
+    pass
+
+
---
+++ releaser/releaser.py
@@ -0,0 +1,60 @@
+#!python
+import sys
+import json
+
+import collect
+from github import Github
+usage = "usage: python releaser.py list|pull [-m milestone] [-c config_file] [-L label1,label2,...]"
+
+class PR:
+    def __init__(self, repo, number, author, title, content, link):
+        self.repo = repo
+        self.number = number
+        self.author = author
+        self.title = title
+        self.content = content 
+        self.link = "github.com/" + repo + "/pull/" + number
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print(usage) # TODO help
+        return 1
+
+    milestone = None
+    labels = None
+    config_path = "../config.json"
+
+    if not args[0] in ["list", "pull"]:
+        print("Please specify the functionality:\n\n",
+            "\tlist : Preview the the generated pr and print the log\n",
+            "\tpull : Generate the release note and open a pr in pingcap/docs\n\n",
+            usage)
+        return 1
+
+    i = 1
+    while i < len(args):
+        if args[i] == "-m":
+            milestone = args[i + 1]
+            i += 1
+        elif args[i] == "-c":
+            config_path = args[i + 1]
+            i += 1
+        elif args[i] == "-L":
+            labels = args[i + 1].split(',')
+            i += 1
+        i += 1
+    config = json.load(open(config_path))
+
+    if milestone == None and labels == None:
+        print(usage)
+
+
+    if args[0] == "list":
+        collect.list(milestone, labels, config["repos"])
+    elif args[0] == "pull":
+        collect.pull(milestone, labels, config["repos"])
+
+if __name__ == "__main__":
+    main()
